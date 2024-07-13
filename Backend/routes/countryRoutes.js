effb09f94d0d147f85d70103ca5c0a30e3516c7a
abhaykawle2010-2010 <aabhhayy2010@gmail.com>
2024-07-13 17:01:31 +0530

    done

--- Backend/routes/countryRoutes.js
+++ Backend/routes/countryRoutes.js
@@ -1,31 +1,36 @@
 const express = require('express');
 const countryRouter = express.Router();
-const {CountryModel} = require('../models/countryModel'); 
-const axios = require('axios'); 
+const { CountryModel } = require('../models/countryModel');
+const axios = require('axios');
 const { auth } = require('../middleware/auth');
 
-
-countryRouter.get('/:currencyCode',auth, async (req, res) => {
+countryRouter.get('/:currencyCode', auth, async (req, res) => {
   try {
-    const currencyCode = req.params.currencyCode.toUpperCase(); 
+    const currencyCode = req.params.currencyCode.toUpperCase();
+
     const countries = await CountryModel.find({ currency: currencyCode });
 
     if (countries.length === 0) {
-
       const apiUrl = `https://restcountries.com/v3.1/currency/${currencyCode}`;
       const apiResponse = await axios.get(apiUrl);
       const apiData = apiResponse.data;
 
       if (apiData.length > 0) {
-        const countryCode = req.params.currencyCode.toUpperCase().substring(0, 2); 
+        // Extract first two characters for flag URL
+        let cc;
+          cc = currencyCode.substring(0, 2);
+       
+
+        const flagUrl = `https://flagsapi.com/${cc}/shiny/64.png`; // Construct flag URL based on cc
 
         const newCountry = new CountryModel({
           name: apiData[0].name.common,
           capital: apiData[0].capital[0],
-          currency: currencyCode,
+          currency: currencyCode, // Use full currency code
           languages: Object.values(apiData[0].languages),
-          flag: `https://flagsapi.com/${countryCode}/shiny/64.png`,
+          flag: flagUrl,
         });
+
         await newCountry.save();
         res.status(200).json(newCountry);
       } else {
@@ -35,10 +40,9 @@ countryRouter.get('/:currencyCode',auth, async (req, res) => {
       res.status(200).json(countries);
     }
   } catch (error) {
-
-    res.status(500).json({ error:error, message: 'Failed to retrieve countries' });
+    console.error(error);
+    res.status(500).json({ error: error.message, message: 'Failed to retrieve countries' });
   }
 });
 
-
-module.exports = {countryRouter};
+module.exports = { countryRouter };
